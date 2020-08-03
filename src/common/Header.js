8e1cd46d329a28d91aac263b5c46f0fa89007a2f
wonjongin <59444360+wonjongin@users.noreply.github.com>
2020-08-03 21:48:19 +0900

    first commit

---
+++ src/common/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="blindnum">숫자 뽑기</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
